914b6abec3893452363ea876f3db55c5522908f6
dkhiati <dkhiati@hotmail.com>
2014-02-20 16:59:43 +0100

    Version 1.0 WifiQR
    First version of the Wifi qrcode generator:
    WifiQR

---
+++ index.js
@@ -0,0 +1,15 @@
+﻿
+window.WifiQR = {};
+
+$(function() {
+
+    WifiQR.app = new DevExpress.framework.html.HtmlApplication({
+		navigationType: "empty",
+        viewPort: {
+            allowPan: true
+		},
+		namespace: WifiQR
+    });
+    WifiQR.app.router.register(":view/:id", { view: "home", id: undefined });
+    WifiQR.app.navigate();   
+});
---
+++ views/home.js
@@ -0,0 +1,35 @@
+﻿var ssid = ko.observable("");
+var wachtwoord = ko.observable("");
+var keuze = ko.observable(0);
+var beveiliging = [
+	{naam: "geen",keuze:1},
+	{naam: "WEP",keuze:2},
+	{naam: "WPA/WPA2",keuze:3}
+	];
+
+var qrinfo = ko.observable("test");
+
+WifiQR.home = function(){
+	
+	var viewModel = {
+		
+		Genereer: function(){
+				if(keuze()==1){
+					qrinfo("WIFI:T:'nopass';S:"+ssid()+";P:'';;");
+				} else if(keuze()==2){
+					qrinfo("WIFI:T:WEP;S:"+ssid()+";P:"+wachtwoord()+";;");
+				} else if(keuze()==3){
+					qrinfo("WIFI:T:WPA;S:"+ssid()+";P:"+wachtwoord()+";;");
+				}
+				alert(qrinfo());
+				$("#qrcode").empty();
+				$("#qrcode").qrcode({
+					render: "table",
+					text: qrinfo()
+				});
+			
+			
+		}
+	};
+	return viewModel;
+};
